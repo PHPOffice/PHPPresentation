e484e2aad140842d1744b9cdc477c3e0b3512aba
Ivan Lanin <ivanlanin@gmail.com>
2014-06-22 19:42:03 +0700

    Update docblock

--- tests/PhpPowerpoint/Tests/Style/AlignmentTest.php
+++ tests/PhpPowerpoint/Tests/Style/AlignmentTest.php
@@ -1,10 +1,18 @@
 <?php
 /**
- * PHPPowerPoint
+ * This file is part of PHPPowerPoint - A pure PHP library for reading and writing
+ * presentations documents.
  *
+ * PHPPowerPoint is free software distributed under the terms of the GNU Lesser
+ * General Public License version 3 as published by the Free Software Foundation.
+ *
+ * For the full copyright and license information, please read the LICENSE
+ * file that was distributed with this source code. For the full list of
+ * contributors, visit https://github.com/PHPOffice/PHPPowerPoint/contributors.
+ *
+ * @copyright   2009-2014 PHPPowerPoint contributors
+ * @license     http://www.gnu.org/licenses/lgpl.txt LGPL version 3
  * @link        https://github.com/PHPOffice/PHPPowerPoint
- * @copyright   2014 PHPPowerPoint
- * @license     http://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt LGPL
  */
 
 namespace PhpOffice\PhpPowerpoint\Tests;
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,19 +1,19 @@
 <?php
 /**
  * This file is part of PHPPowerPoint - A pure PHP library for reading and writing
-* presentation documents.
-*
-* PHPPowerPoint is free software distributed under the terms of the GNU Lesser
-* General Public License version 3 as published by the Free Software Foundation.
-*
-* For the full copyright and license information, please read the LICENSE
-* file that was distributed with this source code. For the full list of
-* contributors, visit https://github.com/PHPOffice/PHPPowerPoint/contributors. test bootstrap
-*
-* @link        https://github.com/PHPOffice/PHPPowerPoint
-* @copyright   2010-2014 PHPPowerPoint contributors
-* @license     http://www.gnu.org/licenses/lgpl.txt LGPL version 3
-*/
+ * presentations documents.
+ *
+ * PHPPowerPoint is free software distributed under the terms of the GNU Lesser
+ * General Public License version 3 as published by the Free Software Foundation.
+ *
+ * For the full copyright and license information, please read the LICENSE
+ * file that was distributed with this source code. For the full list of
+ * contributors, visit https://github.com/PHPOffice/PHPPowerPoint/contributors.
+ *
+ * @copyright   2010-2014 PHPPowerPoint contributors
+ * @license     http://www.gnu.org/licenses/lgpl.txt LGPL version 3
+ * @link        https://github.com/PHPOffice/PHPPowerPoint
+ */
 
 date_default_timezone_set('UTC');
 
