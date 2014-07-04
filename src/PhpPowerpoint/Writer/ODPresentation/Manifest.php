19ce6164a642bfb701a4d30136b3cfbb991f7013
Progi1984 <progi1984@gmail.com>
2014-07-04 16:26:50 +0200

    IMPROVED : Unit Tests

--- src/PhpPowerpoint/Writer/ODPresentation/Manifest.php
+++ src/PhpPowerpoint/Writer/ODPresentation/Manifest.php
@@ -75,28 +75,26 @@ class Manifest extends AbstractPart
         $objWriter->endElement();
 
         $arrMedia = array();
-        if ($parentWriter instanceof ShapeDrawing || $parentWriter instanceof MemoryDrawing) {
-            for ($i = 0; $i < $parentWriter->getDrawingHashTable()->count(); ++$i) {
-                if ($parentWriter->getDrawingHashTable()->getByIndex($i) instanceof ShapeDrawing) {
-                    if (!in_array(md5($parentWriter->getDrawingHashTable()->getByIndex($i)->getPath()), $arrMedia)) {
-                        $arrMedia[] = md5($parentWriter->getDrawingHashTable()->getByIndex($i)->getPath());
-                        $mimeType   = $this->getImageMimeType($parentWriter->getDrawingHashTable()->getByIndex($i)->getPath());
-    
-                        $objWriter->startElement('manifest:file-entry');
-                        $objWriter->writeAttribute('manifest:media-type', $mimeType);
-                        $objWriter->writeAttribute('manifest:full-path', 'Pictures/' . md5($parentWriter->getDrawingHashTable()->getByIndex($i)->getPath()) . '.' . $parentWriter->getDrawingHashTable()->getByIndex($i)->getExtension());
-                        $objWriter->endElement();
-                    }
-                } elseif ($parentWriter->getDrawingHashTable()->getByIndex($i) instanceof MemoryDrawing) {
-                    if (!in_array(str_replace(' ', '_', $parentWriter->getDrawingHashTable()->getByIndex($i)->getIndexedFilename()), $arrMedia)) {
-                        $arrMedia[] = str_replace(' ', '_', $parentWriter->getDrawingHashTable()->getByIndex($i)->getIndexedFilename());
-                        $mimeType = $parentWriter->getDrawingHashTable()->getByIndex($i)->getMimeType();
-    
-                        $objWriter->startElement('manifest:file-entry');
-                        $objWriter->writeAttribute('manifest:media-type', $mimeType);
-                        $objWriter->writeAttribute('manifest:full-path', 'Pictures/' . str_replace(' ', '_', $parentWriter->getDrawingHashTable()->getByIndex($i)->getIndexedFilename()));
-                        $objWriter->endElement();
-                    }
+        for ($i = 0; $i < $parentWriter->getDrawingHashTable()->count(); ++$i) {
+            if ($parentWriter->getDrawingHashTable()->getByIndex($i) instanceof ShapeDrawing) {
+                if (!in_array(md5($parentWriter->getDrawingHashTable()->getByIndex($i)->getPath()), $arrMedia)) {
+                    $arrMedia[] = md5($parentWriter->getDrawingHashTable()->getByIndex($i)->getPath());
+                    $mimeType   = $this->getImageMimeType($parentWriter->getDrawingHashTable()->getByIndex($i)->getPath());
+
+                    $objWriter->startElement('manifest:file-entry');
+                    $objWriter->writeAttribute('manifest:media-type', $mimeType);
+                    $objWriter->writeAttribute('manifest:full-path', 'Pictures/' . md5($parentWriter->getDrawingHashTable()->getByIndex($i)->getPath()) . '.' . $parentWriter->getDrawingHashTable()->getByIndex($i)->getExtension());
+                    $objWriter->endElement();
+                }
+            } elseif ($parentWriter->getDrawingHashTable()->getByIndex($i) instanceof MemoryDrawing) {
+                if (!in_array(str_replace(' ', '_', $parentWriter->getDrawingHashTable()->getByIndex($i)->getIndexedFilename()), $arrMedia)) {
+                    $arrMedia[] = str_replace(' ', '_', $parentWriter->getDrawingHashTable()->getByIndex($i)->getIndexedFilename());
+                    $mimeType = $parentWriter->getDrawingHashTable()->getByIndex($i)->getMimeType();
+
+                    $objWriter->startElement('manifest:file-entry');
+                    $objWriter->writeAttribute('manifest:media-type', $mimeType);
+                    $objWriter->writeAttribute('manifest:full-path', 'Pictures/' . str_replace(' ', '_', $parentWriter->getDrawingHashTable()->getByIndex($i)->getIndexedFilename()));
+                    $objWriter->endElement();
                 }
             }
         }
---
+++ tests/PhpPowerpoint/Tests/Writer/ODPresentation/ContentTest.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * This file is part of PHPPowerPoint - A pure PHP library for reading and writing
+ * presentations documents.
+ *
+ * PHPPowerPoint is free software distributed under the terms of the GNU Lesser
+ * General Public License version 3 as published by the Free Software Foundation.
+ *
+ * For the full copyright and license information, please read the LICENSE
+ * file that was distributed with this source code. For the full list of
+ * contributors, visit https://github.com/PHPOffice/PHPPowerPoint/contributors.
+ *
+ * @copyright   2009-2014 PHPPowerPoint contributors
+ * @license     http://www.gnu.org/licenses/lgpl.txt LGPL version 3
+ * @link        https://github.com/PHPOffice/PHPPowerPoint
+ */
+
+namespace PhpOffice\PhpPowerpoint\Tests\Writer\ODPresentation;
+
+use PhpOffice\PhpPowerpoint\PhpPowerpoint;
+use PhpOffice\PhpPowerpoint\Writer\ODPresentation;
+use PhpOffice\PhpPowerpoint\Tests\TestHelperDOCX;
+use PhpOffice\PhpPowerpoint\Shape\RichText\Run;
+
+/**
+ * Test class for PhpOffice\PhpPowerpoint\Writer\ODPresentation\Manifest
+ *
+ * @coversDefaultClass PhpOffice\PhpPowerpoint\Writer\ODPresentation\Manifest
+ */
+class ContentTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Executed before each method of the class
+     */
+    public function tearDown()
+    {
+        TestHelperDOCX::clear();
+    }
+    
+    public function testTable()
+    {
+        $phpPowerPoint = new PhpPowerpoint();
+        $oSlide = $phpPowerPoint->getActiveSlide();
+        $oShape = $oSlide->createTableShape();
+        
+        $pres = TestHelperDOCX::getDocument($phpPowerPoint, 'ODPresentation');
+        $element = '/office:document-content/office:body/office:presentation/draw:page/draw:frame/table:table';
+        $this->assertTrue($pres->elementExists($element, 'content.xml'));
+    }
+    
+    public function testTableWithColspan()
+    {
+        $value = rand(2, 100);
+        
+        $phpPowerPoint = new PhpPowerpoint();
+        $oSlide = $phpPowerPoint->getActiveSlide();
+        $oShape = $oSlide->createTableShape($value);
+        $oRow = $oShape->createRow();
+        $oCell = $oRow->getCell();
+        $oCell->setColSpan($value);
+        
+        $pres = TestHelperDOCX::getDocument($phpPowerPoint, 'ODPresentation');
+        $element = '/office:document-content/office:body/office:presentation/draw:page/draw:frame/table:table/table:table-row/table:table-cell';
+        $this->assertTrue($pres->elementExists($element, 'content.xml'));
+        $this->assertEquals($value, $pres->getElementAttribute($element, 'table:number-columns-spanned', 'content.xml'));
+    }
+    
+    public function testTableWithText()
+    {
+        $oRun = new Run();
+        $oRun->setText('Test');
+        
+        $phpPowerPoint = new PhpPowerpoint();
+        $oSlide = $phpPowerPoint->getActiveSlide();
+        $oShape = $oSlide->createTableShape();
+        $oRow = $oShape->createRow();
+        $oCell = $oRow->getCell();
+        $oCell->addText($oRun);
+        
+        $pres = TestHelperDOCX::getDocument($phpPowerPoint, 'ODPresentation');
+        $element = '/office:document-content/office:body/office:presentation/draw:page/draw:frame/table:table/table:table-row/table:table-cell/text:p/text:span';
+        $this->assertTrue($pres->elementExists($element, 'content.xml'));
+        $this->assertEquals('Test', $pres->getElement($element, 'content.xml')->nodeValue);
+    }
+
+    public function testList()
+    {
+        
+    }
+}
