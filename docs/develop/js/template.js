05066769dfa88f0db5be849484c142f439ce36d3
travis-ci <travis@travis-ci.org>
2015-01-07 11:54:40 +0000

    PHPDocumentor (Travis Build: 219  - Branch: develop)

--- docs/develop/js/template.js
+++ docs/develop/js/template.js
@@ -9,7 +9,7 @@ function initializeContents()
     // hide all more buttons because they are not needed with JS
     $(".element a.more").hide();
 
-    $(".clickable.class,.clickable.interface").click(function() {
+    $(".clickable.class,.clickable.interface,.clickable.trait").click(function() {
         document.location = $("a.more", this).attr('href');
     });
 
@@ -17,7 +17,7 @@ function initializeContents()
     // do a background color change on hover to emphasize the clickability eveb more
     // we do not use CSS for this because when JS is disabled this behaviour does not
     // apply and we do not want the hover
-    $(".element.method,.element.function,.element.class.clickable,.element.interface.clickable,.element.property.clickable")
+    $(".element.method,.element.function,.element.class.clickable,.element.interface.clickable,.element.trait.clickable,.element.property.clickable")
         .css("cursor", "pointer")
         .hover(function() {
             $(this).css('backgroundColor', '#F8FDF6')
