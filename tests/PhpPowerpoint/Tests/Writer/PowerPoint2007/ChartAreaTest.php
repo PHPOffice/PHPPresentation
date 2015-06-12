2b38819fadf3ec0213a8df00a5c1afd82dd855b5
Progi1984 <progi1984@gmail.com>
2015-06-12 21:06:12 +0200

    PowerPoint2007 Writer : Support for Area Chart - @Progi1984 GH-82

--- tests/PhpPowerpoint/Tests/Writer/PowerPoint2007/ChartAreaTest.php
+++ tests/PhpPowerpoint/Tests/Writer/PowerPoint2007/ChartAreaTest.php
@@ -69,9 +69,5 @@ class ChartAreaTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($oXMLDoc->elementExists($element, 'ppt/charts/'.$oShape->getIndexedFilename()));
         $element = '/c:chartSpace/c:chart/c:plotArea/c:areaChart/c:ser';
         $this->assertTrue($oXMLDoc->elementExists($element, 'ppt/charts/'.$oShape->getIndexedFilename()));
-        $element = '/c:chartSpace/c:chart/c:plotArea/c:lineChart/c:ser/c:tx/c:v';
-        $this->assertTrue($oXMLDoc->elementExists($element, 'ppt/charts/'.$oShape->getIndexedFilename()));
-        $element = '/c:chartSpace/c:chart/c:plotArea/c:areaChart/c:ser/c:tx/c:v';
-        $this->assertEquals($oSeries->getTitle(), $oXMLDoc->getElement($element, 'ppt/charts/'.$oShape->getIndexedFilename())->nodeValue);
     }
 }
