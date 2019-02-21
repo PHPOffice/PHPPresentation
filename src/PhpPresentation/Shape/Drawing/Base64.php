c277b3a3d1f72edc322e15f439f37f472c7e333e
Aggiekev <kevin@nextafter.com>
2019-02-21 07:00:17 -0600

    Update Base64.php

--- src/PhpPresentation/Shape/Drawing/Base64.php
+++ src/PhpPresentation/Shape/Drawing/Base64.php
@@ -22,6 +22,7 @@ class Base64 extends AbstractDrawingAdapter
     protected $arrayMimeExtension = array(
         'image/jpeg' => 'jpg',
         'image/png'  => 'png',
+        'image/gif'  => 'gif',
     );
 
     /**
