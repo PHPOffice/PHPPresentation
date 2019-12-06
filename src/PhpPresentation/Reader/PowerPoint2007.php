29c2f5c2aaff77f7d503a868e5484516aef18034
Jean-Baptiste Noblot <jean-baptiste@grytics.com>
2019-12-06 10:46:06 +0100

    Useless use of new DateTime() and rewrite after with static call

--- src/PhpPresentation/Reader/PowerPoint2007.php
+++ src/PhpPresentation/Reader/PowerPoint2007.php
@@ -217,8 +217,7 @@ class PowerPoint2007 implements ReaderInterface
                 $oElement = $xmlReader->getElement($path);
                 if ($oElement instanceof \DOMElement) {
                     if ($oElement->hasAttribute('xsi:type') && $oElement->getAttribute('xsi:type') == 'dcterms:W3CDTF') {
-                        $oDateTime = new \DateTime();
-                        $oDateTime->createFromFormat(\DateTime::W3C, $oElement->nodeValue);
+                        $oDateTime = \DateTime::createFromFormat(\DateTime::W3C, $oElement->nodeValue);
                         $oProperties->{$property}($oDateTime->getTimestamp());
                     } else {
                         $oProperties->{$property}($oElement->nodeValue);
