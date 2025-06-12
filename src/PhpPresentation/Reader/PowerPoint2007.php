9385cabdbefb2710d19e7d9c1fc0170da4dcb6bf
Phil Wilkinson <phil.wilkinson@gomolearning.com>
2025-06-12 12:42:06 +0100

    Added support for loading text run without properties

--- src/PhpPresentation/Reader/PowerPoint2007.php
+++ src/PhpPresentation/Reader/PowerPoint2007.php
@@ -1546,9 +1546,9 @@ class PowerPoint2007 implements ReaderInterface
             }
             if ('a:r' == $oSubElement->tagName) {
                 $oElementrPr = $document->getElement('a:rPr', $oSubElement);
-                if (is_object($oElementrPr)) {
-                    $oText = $oParagraph->createTextRun();
 
+                $oText = $oParagraph->createTextRun();
+                if (is_object($oElementrPr)) {
                     if ($oElementrPr->hasAttribute('b')) {
                         $att = $oElementrPr->getAttribute('b');
                         $oText->getFont()->setBold('true' == $att || '1' == $att ? true : false);
@@ -1626,10 +1626,9 @@ class PowerPoint2007 implements ReaderInterface
                             $oText->getFont()->setCharset((int) $oElementFont->getAttribute('charset'));
                         }
                     }
-
-                    $oSubSubElement = $document->getElement('a:t', $oSubElement);
-                    $oText->setText($oSubSubElement->nodeValue);
                 }
+                $oSubSubElement = $document->getElement('a:t', $oSubElement);
+                $oText->setText($oSubSubElement->nodeValue);
             }
         }
     }
